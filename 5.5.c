dee18c0be79014cfd4b4aa28f3a8c1c99a2601dd
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-23 15:32:58 +0900

    Add 5 chapter file

---
+++ 5.5.c
@@ -0,0 +1,13 @@
+// C언어 코딩 도장
+// Chapter 5 연습문제
+
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 10, num2 = 20, num3 = 30;
+
+    printf("%d %d %d\n", num1, num2, num3);
+
+    return 0;
+}
---
+++ 5.6.c
@@ -0,0 +1,16 @@
+// C언어 코딩 도장
+// Chapter 5 심사문제
+
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+
+    num1 = 10;
+    int num2 = 20, num3 = 30;
+
+    printf("%d %d %d\n", num1, num2, num3);
+
+    return 0;
+}
