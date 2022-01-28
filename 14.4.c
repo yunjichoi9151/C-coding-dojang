0562979b7680ae6d2fced21cc481ea2edd7fd574
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-28 20:52:15 +0900

    Add 14 chapter file

---
+++ 14.4.c
@@ -0,0 +1,17 @@
+// C언어 코딩 도장
+// Chapter 14 연습문제
+
+#include <stdio.h>
+
+int main()
+{
+    int base = 20;
+    int height = 16;
+    int area;
+
+    area = base * height / 2;
+
+    printf("%d\n", area);
+
+    return 0;
+}
