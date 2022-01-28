fca05357fd714f6821af9e69beb249011edea640
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-28 21:36:05 +0900

    Add 16 chapter file

---
+++ 16.4.c
@@ -0,0 +1,14 @@
+// C언어 코딩 도장
+// Chapter 16 연습문제
+
+#include <stdio.h>
+
+int main()
+{
+    char c1 = 'a';
+    char num2 = c1;
+
+    printf("%c\n", num2);
+
+    return 0;
+}
---
+++ 16.5.c
@@ -0,0 +1,18 @@
+// C언어 코딩 도장
+// Chapter 16 심사문제
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    float num1;
+
+    scanf("%f", &num1);
+
+    int num2 = num1;
+
+    printf("%d\n", num2);
+
+    return 0;
+}
