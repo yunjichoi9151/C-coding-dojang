541ff93ec63a8e0d4df9e059ad9c31951ada479f
yunjichoi9151 <cyjcode99@gmail.com>
2022-02-09 19:57:33 +0900

    Add 26 chapter file

---
+++ 26.7.c
@@ -0,0 +1,30 @@
+// C언어 코딩 도장
+// Chapter 26 연습문제
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 0;
+
+    scanf("%d", &num1);
+
+    switch (num1)
+    {
+    case 1 << 1:
+        printf("2\n");
+        break;
+    case 1 << 2:
+        printf("4\n");
+        break;
+    case 1 << 3:
+        printf("8\n");
+        break;
+    default:
+        printf("default\n");
+        break;
+    }
+
+    return 0;
+}
