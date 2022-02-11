f3b0dd4ec38f71955cabcbbffc02354960a785a5
yunjichoi9151 <cyjcode99@gmail.com>
2022-02-11 14:15:53 +0900

    Add 35 chapter file

---
+++ 35.6.c
@@ -0,0 +1,22 @@
+// C언어 코딩 도장
+// Chapter 35 연습문제
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main()
+{
+    int *numPtr1 = malloc(sizeof(int));
+    long long *numPtr2 = malloc(sizeof(long long));
+
+    *numPtr1 = INT_MAX;
+    *numPtr2 = LLONG_MAX;
+
+    printf("%d %lld\n", *numPtr1, *numPtr2);
+
+    free(numPtr1);
+    free(numPtr2);
+
+    return 0;
+}
---
+++ 35.7.c
@@ -0,0 +1,27 @@
+// C언어 코딩 도장
+// Chapter 35 심사문제
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1;
+    int num2;
+
+    int *numPtr1 = malloc(sizeof(int));
+    int *numPtr2 = malloc(sizeof(int));
+
+    scanf("%d %d", &num1, &num2);
+
+    *numPtr1 = num1;
+    *numPtr2 = num2;
+
+    printf("%d\n", *numPtr1 + *numPtr2);
+
+    free(numPtr1);
+    free(numPtr2);
+
+    return 0;
+}
