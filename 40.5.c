5338de7431acf30c3e06facbd1ac0072b49ec5b6
yunjichoi9151 <cyjcode99@gmail.com>
2022-02-13 14:08:00 +0900

    Add 40 chapter file

---
+++ 40.5.c
@@ -0,0 +1,17 @@
+// C언어 코딩 도장
+// Chapter 40 연습문제
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    char s1[10];
+
+    printf("문자열을 입력하세요: ");
+    scanf("%s", s1);
+
+    printf("%s\n", s1);
+
+    return 0;
+}
---
+++ 40.6.c
@@ -0,0 +1,20 @@
+// C언어 코딩 도장
+// Chapter 40 연습문제
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *s1 = malloc(sizeof(char) * 10);
+
+    printf("문자열을 입력하세요: ");
+    scanf("%s", s1);
+
+    printf("%s\n", s1);
+
+    free(s1);
+
+    return 0;
+}
