b60ed8384c86c557af38ae9d80692c81c500e449
yunjichoi9151 <cyjcode99@gmail.com>
2022-02-17 00:58:30 +0900

    Add 52 chapter file

---
+++ 52.4.c
@@ -0,0 +1,26 @@
+// C언어 코딩 도장
+// Chapter 52 연습문제
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Point2D {
+    int x;
+    int y;
+};
+
+int main()
+{
+    struct Point2D p;
+    struct Point2D *ptr = malloc(sizeof(struct Point2D));
+
+    memset(&p, 0, sizeof(struct Point2D));
+    memset(ptr, 0, sizeof(struct Point2D));
+
+    printf("%d %d %d %d\n", p.x, p.y, ptr->x, ptr->y);
+
+    free(ptr);
+
+    return 0;
+}
